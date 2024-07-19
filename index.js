0de9dd61df94b5559e607cc3f53f3c97910148ac
ALFAT GILMANSHIN <89654590+Alfat1989@users.noreply.github.com>
2024-07-19 20:56:00 +0500

    feat: add button t-bank (#1)

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-const authButton = document.querySelector('#auth-button')
-const clientId = 'tid_ligafootball'
-const clientSecret = 'sNo4lTQ7MiZxsb9Ys9cxzklm0G2Wkr'
-const redirectUri = './index2.html'
+// const authButton = document.querySelector('#auth-button')
+// const clientId = 'tid_ligafootball2'
+// const clientSecret = 'sNo4lTQ7MiZxsb9Ys9cxzklm0G2Wkr'
+// const redirectUrl = window.location.origin + './index2.html'
 
-authButton.addEventListener('click', authFn)
+// authButton.addEventListener('click', authFn2)
 
-function authFn() {
-    const state = generateState();
-    const authUrl = `https://id.tbank.ru/auth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&state=${state}`;
-    const authWindow = window.open(authUrl, 'authWindow', 'width=800,height=600');
-    const checkAuthWindowClosed = setInterval(function () {
-        if (authWindow.closed) {
-            clearInterval(checkAuthWindowClosed);
-            // Обработка получения кода из redirectUri
-            const urlParams = new URLSearchParams(window.location.search);
-            const authCode = urlParams.get('code');
+// function authFn() {
+//     const state = generateState();
+//     const authUrl = `https://id.tbank.ru/auth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUrl}&state=${state}`;
+//     const authWindow = window.open(authUrl, 'authWindow', 'width=800,height=600');
+//     const checkAuthWindowClosed = setInterval(function () {
+//         if (authWindow.closed) {
+//             clearInterval(checkAuthWindowClosed);
+//             // Обработка получения кода из redirectUrl
+//             const urlParams = new URLSearchParams(window.location.search);
+//             const authCode = urlParams.get('code');
 
-            if (authCode) {
-                // Замените URL на нужный вам URL для обработки успешной авторизации
-                window.location.href = `./welcome.html?code=${authCode}`;
-            } else {
-                alert('Ошибка авторизации');
-            }
-        }
-    }, 1000);
-}
+//             if (authCode) {
+//                 // Замените URL на нужный вам URL для обработки успешной авторизации
+//                 window.location.href = `./welcome.html?code=${authCode}`;
+//             } else {
+//                 alert('Ошибка авторизации');
+//             }
+
+//             console.log(authCode);
+//         }
+//     }, 1000);
+// }
+
+// function authFn2() {
+//     const state = generateState();
+//     sessionStorage.setItem('auth_state', state); // Сохраняем state в sessionStorage для проверки после редиректа
+
+//     const authUrl = `https://id.tbank.ru/auth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUrl}&state=${state}`;
+//     window.location.href = authUrl;
+// }
 
 function generateState() {
     return Math.random().toString(36).substring(2); // Простая генерация случайного state
 }
+
+const authParams = {
+    redirectUri: './welcome.html',
+    // redirectUri: 'https://mysite.ru/auth/success',
+    responseType: 'code',
+    clientId: 'tid_ligafootball2',
+    // state: 'YOUR_STATE'
+    state: generateState()
+};
+
+const uiParams = {
+    container: '#container-for-tid-button',
+    size: 'm',
+    color: 'primary',
+    text: 'Т-Банк',
+    target: '_self'
+};
+
+const tidSdk = new TidSDK(authParams);
+tidSdk.addButton(uiParams);
